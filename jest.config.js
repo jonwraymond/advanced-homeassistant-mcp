1fde044a65c91f46924e1876554dcfb01ca82c72
Jonathan Raymond <jonwraymond@gmail.com>
2025-03-12 22:40:55 -0600

    Add Jest configuration

---
+++ jest.config.js
@@ -0,0 +1,18 @@
+// jest.config.js
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testMatch: ['**/tests/**/*.test.ts'],
+  collectCoverageFrom: [
+    'src/**/*.ts',
+    '!src/index.ts'
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80
+    }
+  }
+};
